adf0008571a536d5e3c4e838e4991e2d7d3e1b5b
lijun <gawainli@outlook.com>
2024-10-12 20:56:31 +0800

    chore: 上传基础文件

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EchoTCPServerCSharp.Server;
+
+Console.WriteLine("Run Echo TCP Server");
+TcpEchoServer.Run("127.0.0.1", 8893);
---
+++ Server/TcpEchoServer.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using MiniGame.Network;
+
+namespace EchoTCPServerCSharp.Server;
+
+public class TcpEchoServer
+{
+    public static void Run(string address, int port)
+    {
+        TcpListener listener = new TcpListener(IPAddress.Parse(address), port);
+        listener.Start();
+        Console.WriteLine($"Server started at {address}:{port}");
+
+        try
+        {
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+                Console.WriteLine($"Remote endpoint: {client.Client.RemoteEndPoint}");
+                var channel = new TcpChannel(client, new DefaultPkgEncoder(), new DefaultPkgDecoder());
+                channel.StartProcess();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
